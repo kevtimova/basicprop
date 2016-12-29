7d8ad1f093b6854e3896a8c5fb187112519048ef
Andrew Drozdov <andrew@mrdrozdov.com>
2016-12-29 12:30:09 -0500

    Minimal viable basicprop dataset

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'basicprop',
   packages = ['basicprop'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A synthetic dataset used for generative models',
   author = 'Andrew Drozdov',
   author_email = 'andrew@mrdrozdov.com',
